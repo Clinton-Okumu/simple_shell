53f86f095b9513b2ff7ca91ab0ecdafe5d62d900
Clinton-Okumu <clintonomondiokumu@gmail.com>
2023-11-10 01:45:29 -0800

    lists.c

--- lists1.c
+++ lists1.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
+ * listLen - determines length of linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t list_len(const list_t *h)
+size_t listLen(const list_t *h)
 {
 	size_t i = 0;
 
@@ -19,12 +19,12 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
+ * listToStrings - returns an array of strings of the list->str
  * @head: pointer to first node
  *
  * Return: array of strings
  */
-char **list_to_strings(list_t *head)
+char **listToStrings(list_t *head)
 {
 	list_t *node = head;
 	size_t i = list_len(head), j;
@@ -56,12 +56,12 @@ char **list_to_strings(list_t *head)
 
 
 /**
- * print_list - prints all elements of a list_t linked list
+ * printList - prints all elements of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t print_list(const list_t *h)
+size_t printList(const list_t *h)
 {
 	size_t i = 0;
 
@@ -79,14 +79,14 @@ size_t print_list(const list_t *h)
 }
 
 /**
- * node_starts_with - returns node whose string starts with prefix
+ * nodeStartsWith - returns node whose string starts with prefix
  * @node: pointer to list head
  * @prefix: string to match
  * @c: the next character after prefix to match
  *
  * Return: match node or null
  */
-list_t *node_starts_with(list_t *node, char *prefix, char c)
+list_t *nodeStartsWith(list_t *node, char *prefix, char c)
 {
 	char *p = NULL;
 
@@ -101,13 +101,13 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 }
 
 /**
- * get_node_index - gets the index of a node
+ * getNodeIndex - gets the index of a node
  * @head: pointer to list head
  * @node: pointer to the node
  *
  * Return: index of node or -1
  */
-ssize_t get_node_index(list_t *head, list_t *node)
+ssize_t getNodeIndex(list_t *head, list_t *node)
 {
 	size_t i = 0;
 
--- shell.h
+++ shell.h
@@ -219,11 +219,11 @@ int deleteNodeAtIndex(list_t **, unsigned int);
 void freeList(list_t **);
 
 /* lists1.c */
-size_t list_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
+size_t listLen(const list_t *);
+char **listToStrings(list_t *);
+size_t printList(const list_t *);
+list_t *nodeStarts_with(list_t *, char *, char);
+ssize_t getNodeIndex(list_t *, list_t *);
 
 /* vars.c */
 int is_chain(info_t *, char *, size_t *);
