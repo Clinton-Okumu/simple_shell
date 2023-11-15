0ee0b2deb4623e4b37103fed3f9e8951b191d2ab
Clinton-okumu <clintonomondiokumu@gmail.com>
2023-11-15 06:31:48 +0300

    changes

--- shell_loop.c
+++ shell_loop.c
@@ -9,35 +9,39 @@
  */
 int my_shell_loop(info_t *info, char **argv)
 {
-    ssize_t read_result = 0;
-    int builtin_return = 0;
+	ssize_t read_result = 0;
+	int builtin_return = 0;
 
-    while (read_result != -1 && builtin_return != -2) {
-        clear_info(info);
-        if (interact(info))
-            _puts("$ ");
-        _eputchar(BUF_FLUSH);
-        read_result = get_input(info);
-        if (read_result != -1) {
-            set_info(info, argv);
-            builtin_return = find_builtin_command(info);
-            if (builtin_return == -1)
-                find_executable_command(info);
-        } else if (interact(info)) {
-            _putchar('\n');
-        }
-        free_info(info, 0);
-    }
-    writeHistory(info);
-    free_info(info, 1);
-    if (!interact(info) && info->status)
-        exit(info->status);
-    if (builtin_return == -2) {
-        if (info->err_num == -1)
-            exit(info->status);
-        exit(info->err_num);
-    }
-    return (builtin_return);
+	while (read_result != -1 && builtin_return != -2)
+	{
+		clear_info(info);
+		if (interact(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		read_result = get_input(info);
+		if (read_result != -1)
+		{
+			set_info(info, argv);
+			builtin_return = find_builtin_command(info);
+			if (builtin_return == -1)
+				find_executable_command(info);
+		} else if (interact(info))
+		{
+			_putchar('\n');
+		}
+		free_info(info, 0);
+	}
+	writeHistory(info);
+	free_info(info, 1);
+	if (!interact(info) && info->status)
+		exit(info->status);
+	if (builtin_return == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_return);
 }
 
 /**
@@ -51,27 +55,30 @@ int my_shell_loop(info_t *info, char **argv)
  */
 int find_builtin_command(info_t *info)
 {
-    int i, builtin_return = -1;
-    builtin_table builtintbl[] = {
-        {"myexit", my_exit},
-        {"myenv", my_env},
-        {"myhelp", my_help},
-        {"myhistory", my_history},
-        {"mysetenv", my_setenv},
-        {"myunsetenv", my_unsetenv},
-        {"mycd", my_cd},
-        {"myalias", my_alias},
-        {NULL, NULL}
-    };
+	int i, builtin_return = -1;
+	builtin_table builtintbl[] =
+	{
+		{"myexit", my_exit},
+		{"myenv", my_env},
+		{"myhelp", my_help},
+		{"myhistory", my_history},
+		{"mysetenv", my_setenv},
+		{"myunsetenv", my_unsetenv},
+		{"mycd", my_cd},
+		{"myalias", my_alias},
+		{NULL, NULL}
+	};
 
-    for (i = 0; builtintbl[i].type; i++) {
-        if (my_strcmp(info->argv[0], builtintbl[i].type) == 0) {
-            info->line_count++;
-            builtin_return = builtintbl[i].func(info);
-            break;
-        }
-    }
-    return (builtin_return);
+	for (i = 0; builtintbl[i].type; i++)
+	{
+		if (my_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			builtin_return = builtintbl[i].func(info);
+			break;
+		}
+	}
+	return (builtin_return);
 }
 
 /**
@@ -82,38 +89,44 @@ int find_builtin_command(info_t *info)
  */
 void find_executable_command(info_t *info)
 {
-    char *path = NULL;
-    int i, arg_length = 0;
+	char *path = NULL;
+	int i, arg_length = 0;
 
-    info->path = info->argv[0];
-    if (info->linecount_flag == 1) {
-        info->line_count++;
-        info->linecount_flag = 0;
-    }
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
 
-    for (i = 0; info->arg[i]; i++) {
-        if (!is_delim(info->arg[i], " \t\n")) {
-            arg_length++;
-        }
-    }
+	for (i = 0; info->arg[i]; i++)
+	{
+		if (!is_delim(info->arg[i], " \t\n"))
+		{
+			arg_length++;
+		}
+	}
 
-    if (!arg_length) {
-        return;
-    }
-
-    path = find_command_in_path(info, _getenv(info, "PATH="), info->argv[0]);
-
-    if (path) {
-        info->path = path;
-        fork_executable_command(info);
-    } else {
-        if ((interact(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/') && isCmd(info, info->argv[0])) {
-            fork_executable_command(info);
-        } else if (*(info->arg) != '\n') {
-            info->status = 127;
-            print_error(info, "not found\n");
-        }
-    }
+	if (!arg_length)
+	{
+		return;
+	}
+	path = find_command_in_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_executable_command(info);
+	} else
+	{
+		if ((interact(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/') && isCmd(info, info->argv[0]))
+		{
+			fork_executable_command(info);
+		} else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
 }
 
 /**
@@ -124,29 +137,33 @@ void find_executable_command(info_t *info)
  */
 void fork_executable_command(info_t *info)
 {
-    pid_t child_pid;
+	pid_t child_pid;
 
-    child_pid = fork();
-    if (child_pid == -1) {
-        /* TODO: PUT ERROR FUNCTION */
-        perror("Error:");
-        return;
-    }
-    if (child_pid == 0) {
-        if (execve(info->path, info->argv, get_environ(info)) == -1) {
-            free_info(info, 1);
-            if (errno == EACCES)
-                exit(126);
-            exit(1);
-        }
-        /* TODO: PUT ERROR FUNCTION */
-    } else {
-        wait(&(info->status));
-        if (WIFEXITED(info->status)) {
-            info->status = WEXITSTATUS(info->status);
-            if (info->status == 126) {
-                print_error(info, "Permission denied\n");
-            }
-        }
-    }
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	} else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+			{
+				print_error(info, "Permission denied\n");
+			}
+		}
+	}
 }
